3d68e4e48598ee82e0271c57da4d866035b3ccbe
Matthew Greenwell <dp005352@live.reading.ac.uk>
2018-07-06 10:26:24 +0100

    created graph

--- Sample_script.R
+++ Sample_script.R
@@ -32,13 +32,18 @@ theme.clean <- function(){
           legend.title = element_blank(),                              
           legend.position=c(0.9, 0.9))
 }
-
+library(tidyverse)
 # Load data ----
 setwd("your file path")
 temp_elevation <- read.csv("temp_elevation.csv")
 
 # Make a plot showing how soil temperature changes with elevation ----
 
+(temp.el <- ggplot (temp_elevation, aes(x = Elevation.m, y = Soil.temp.mean)) +
+  geom_point(colour = "#8B4513") +
+  geom_smooth(method = lm, colour = "#8B4513", fill = "#8B4513", alpha = 0.6) +
+  labs(x = "Elevation (m)", y = "Mean soil temperature (°C)"))
+t
 # Save your plot in your project directory
 
 
